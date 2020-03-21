bab0ff6e27974289f6340d9f51ee4e024c369925
eugene-finn <1287175@gmail.com>
2020-03-21 20:41:29 +0200

    tv-maze-show ready set go

--- src/components/Show/index.js
+++ src/components/Show/index.js
@@ -1,1 +1,57 @@
-export { default } from './Show';
+import React from 'react';
+
+class Show extends React.Component {
+  state = {
+    image: '',
+    name: '',
+    genres: '',
+    summary: ''
+  };
+
+  // componentDidMount() {
+  //   this.getTvMaze();
+  // }
+
+  componentDidUpdate() {
+    console.log(this.props.showId);
+    this.getTvMaze(this.props.showId);
+  }
+
+  getTvMaze = async showId => {
+    const response = await fetch(`http://api.tvmaze.com/shows/${showId}`);
+
+    const json = await response.json();
+
+    const {
+      image: { medium: image },
+      name,
+      genres,
+      summary
+    } = json;
+    this.setState({ image, name, genres, summary });
+  };
+
+  render() {
+    console.log(this.state);
+    console.log('props', this.props);
+    return (
+      <div className="show">
+        <img
+          className="show-image"
+          src={this.state.image}
+          alt={this.state.name}
+        />
+        <h2 className="show-label t-show-name">{this.state.name}</h2>
+        <p className="show-text t-show-genre">
+          <p>{this.state.genres}</p>
+        </p>
+        <p
+          className="show-text t-show-summary"
+          dangerouslySetInnerHTML={{ __html: this.state.summary }}
+        ></p>
+      </div>
+    );
+  }
+}
+
+export default Show;
